8caf2e77c0f64bde2b50e2259314cb45d36c37a6
adazhdw <dgz102486@163.com>
2020-03-03 10:18:29 +0800

    add main1.dart

--- firstflutter/lib/main1.dart
+++ firstflutter/lib/main1.dart
@@ -26,7 +26,8 @@ class RandomWords extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
 //    return new RandomWordsState();
-    return new RandomWordsState2();
+//    return new RandomWordsState2();
+    return new RandomWordsState3();
   }
 }
 
@@ -48,23 +49,16 @@ class RandomWordsState2 extends State<RandomWords> {
 
   Widget _buildSuggestions() {
     return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
-        itemBuilder: (context, i) {
-          //在每一列之前，添加一个1像素高的分割线widget,//isOdd 表示是否是奇数
-          if (i.isOdd) return new Divider();
-          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
-          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-          final index = 1 ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs()
-                .take(10)); //generateWordPairs()是english_words.dart库中的方法
-          }
-          return _buildRow(_suggestions[index]);
-        });
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
   }
 
   @override
@@ -88,6 +82,7 @@ class RandomWordsState2 extends State<RandomWords> {
         suggestion.asPascalCase,
         style: _biggerFont,
       ),
+
       ///第5步: 添加交互
       trailing: new Icon(
         alreadySaved ? Icons.favorite : Icons.favorite_border,
@@ -103,5 +98,44 @@ class RandomWordsState2 extends State<RandomWords> {
         });
       },
     );
+  }
+}
+
+///第6步: 导航到新页面
+class RandomWordsState3 extends RandomWordsState2 {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("StartsUp Name Generator"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return new ListTile(
+          title: new Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
+      final divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Saved Suggestions'),
+        ),
+        body: new ListView(
+          children: divided,
+        ),
+      );
+    }));
   }
 }

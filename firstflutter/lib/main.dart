b96f428287e93314933af9b9dfda89d31e0b771b
adazhdw <dgz102486@163.com>
2020-03-05 17:08:39 +0800

    add Eajy/flutter_demo code

--- firstflutter/lib/main.dart
+++ firstflutter/lib/main.dart
@@ -1,20 +1,18 @@
-import 'package:firstflutter/main1.dart';
+import 'package:firstflutter/route/home.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';///第2步: 使用外部包(package)
 
-void main() => runApp(MyApp1());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
-      home: new Scaffold(
-        appBar: new AppBar(title: new Text("Welcome to Flutter"),),
-        body: new Center(child: new Text(wordPair.asPascalCase),),
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.amber,
       ),
+      home: new HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
